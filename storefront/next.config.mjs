2dc670b9dbc920eecc4e58f1db478a18e98b19f6
Ashkan <ashkan.eddyking@gmail.com>
2026-02-12 13:55:49 +0400

    Added my domain to trusted nextjs image optimization.

--- storefront/next.config.mjs
+++ storefront/next.config.mjs
@@ -28,6 +28,18 @@ const nextConfig = {
         protocol: "https",
         hostname: "dummyimage.com",
       },
+      {
+        protocol: "https",
+        hostname: "nextlicense.shop",
+      },
+      {
+        protocol: "https",
+        hostname: "medusa-public-images.s3.eu-west-1.amazonaws.com",
+      },
+      {
+        protocol: "https",
+        hostname: "medusa-server-testing.s3.amazonaws.com",
+      },
     ],
   },
 };
